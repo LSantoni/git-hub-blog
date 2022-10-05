23c6f3042cba1532fc88f898e4d6e3890c69293e
LSantoni <lizandra.santoni@gmail.com>
2022-10-05 18:04:07 -0300

    Estilizar tela de detalhes

--- src/pages/RepositoryDetail/index.tsx
+++ src/pages/RepositoryDetail/index.tsx
@@ -1,5 +1,53 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare, faCalendarDay, faChevronLeft, faComment } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import { Content, DetailContainer, EmphasisContainer, HeaderCard } from "./styles";
+
 export function RepositoryDetail() { 
   return (
-    <div>Details</div>
+    <DetailContainer>
+      <HeaderCard>
+        <div>
+          <button>
+            <FontAwesomeIcon icon={faChevronLeft} />
+            <span>VOLTAR</span>
+          </button>
+          <button>
+            <span>VER NO GITHUB</span>
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+          </button>
+        </div>
+        <span id="header-title">JavaScript data types and data structures</span>
+        <footer>
+          <div>
+            <FontAwesomeIcon icon={faGithub} />
+            <span>cameronwll</span>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faCalendarDay} />
+            <span>Rocketseat</span>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faComment} />
+            <span>32 seguidores</span>
+          </div>
+        </footer>
+      </HeaderCard>
+      <Content>
+        <p>
+          Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.
+        </p>
+        <p>
+          Dynamic typing
+          JavaScript is a loosely typed and dynamic language. Variables in JavaScript are not directly associated with any particular value type, and any variable can be assigned (and re-assigned) values of all types:
+        </p>
+        <EmphasisContainer>
+          let foo = 42;   // foo is now a number <br />
+          foo = ‘bar’;    // foo is now a string <br />
+          foo = true;     // foo is now a boolean
+        </EmphasisContainer>
+      </Content>
+    </DetailContainer>
   )
 }
--- src/pages/RepositoryDetail/styles.ts
+++ src/pages/RepositoryDetail/styles.ts
@@ -1,3 +1,69 @@
 import styled from "styled-components";
 
-export const DetailContainer = styled.div``;
+export const DetailContainer = styled.div`
+  max-width: 54rem;
+  margin: 0 auto;
+  padding-bottom: 5rem;
+`;
+
+export const HeaderCard = styled.header`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  background: ${props => props.theme["base-profile"]};
+  border-radius: 10px;
+  margin-top: -5.5rem;
+
+  div:first-of-type {
+    display: flex;
+    justify-content: space-between;
+
+    button {
+      background: transparent;
+      border: none;
+      font-weight: 700;
+      font-size: 0.75rem;
+      color: ${props => props.theme.blue};
+      display: flex;
+      gap: 0.5rem;
+    }
+  }
+
+  #header-title {
+    margin: 1.25rem 0 0.5rem;
+    font-size: 1.5rem;
+  }
+
+  footer {
+    display: flex;
+
+    div {
+      display: flex;
+      gap: 0.5rem;
+
+      svg {
+        width: 18px;
+        height: 18px;
+      }
+
+      & + div{
+        margin-left: 1.5rem;
+      }
+    }
+  }
+`;
+
+export const Content = styled.section`
+  padding: 2.5rem;
+  font-size: 1rem;
+
+  p + p {
+    margin: 2rem 0;
+  }
+`;
+
+export const EmphasisContainer = styled.div`
+  background: ${props => props.theme["base-post"]};
+  padding: 1rem;
+  border-radius: 2px;
+`;
